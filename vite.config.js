594affc46c346836afd72ef78dd82eb4714c4c07
PeXxa <yassinejeridi23@gmail.com>
2025-10-23 00:58:37 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/redix-furniture-portfolio/',
-  publicDir: 'public',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    copyPublicDir: true
-  }
 })
